2e4c9945a440a7d24c66ee8a54700d4f5cf4df0e
puspus-dev <boszormenyiadam03@gmail.com>
2026-09-19 07:31:20 +0200

    Update fix-pub.sh

--- fix-pub.sh
+++ fix-pub.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-# Hibás submodule ne zavarjon
 rm -rf filcnaplo_premium 2>/dev/null || true
 
 pub_get() {
@@ -10,11 +9,10 @@ pub_get() {
     echo "==> $dir"
     (cd "$dir" && flutter clean && flutter pub get)
   else
-    echo "==> SKIP $dir (nincs pubspec.yaml)"
+    echo "==> SKIP $dir"
   fi
 }
 
-# Sorrend: előbb a path-függőségek, aztán a fő app
 pub_get "naplo_premium"
 pub_get "filcnaplo_kreta_api"
 pub_get "filcnaplo_mobile_ui"
